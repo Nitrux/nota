dda4b22bc7e51bc2bde69ac549e5b40563c3f221
Uri Herrera <hello@uriherrera.com>
2025-06-20 19:54:37 -0600

    ffs

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -37,6 +37,8 @@ git clone --depth 1 --branch "$NOTA_BRANCH" https://invent.kde.org/maui/nota.git
 
 mkdir -p build && cd build
 
+HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
+
 cmake \
 	-DCMAKE_INSTALL_PREFIX=/usr \
 	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
